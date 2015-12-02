3cbe59e544c7090d4b4a053c937fcdbd46571ab2
Mike Wickwar <mike.wickwar@gmail.com>
2015-12-02 16:58:58 -0700

    adding images now on second go

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var button = document.getElementById('button');
 var iny = document.getElementById('sItem');
 button.addEventListener('click',function(){
 
-
 var getter = $.ajax({
   url: 'https://www.reddit.com/subreddits/search.json?q=' +iny.value,
   method: "GET",
@@ -11,5 +10,18 @@ var getter = $.ajax({
 
 getter.done(function(responce){
   console.log(responce);
+  var a = responce.data.children;
+  for (var i = 0; i < a.length; i++) {
+    console.log(a[i].data.display_name);  //test
+    $('#tabl').append("<tr><td>"+a[i].data.display_name+"</td></tr>")
+  }
+  for (var i = 0; i < a.length; i++) {
+    console.log(a[i].data.header_img);
+    $('#tabl').append("<tr><td><image src='"+a[i].data.header_img"'/></td></tr>")
+  }
+
 });
 });
+
+
+//start with thte error on ln 20
