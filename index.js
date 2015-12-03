8f0fe09bb3b24fe643eb2b9109c48f6398866519
Mike Wickwar <mike.wickwar@gmail.com>
2015-12-02 19:33:59 -0700

    getting better added some css

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var button = document.getElementById('button');
 var iny = document.getElementById('sItem');
+$(document).ready(function(){
+  $("#tabl").hide();
+})
 button.addEventListener('click',function(){
 
 var getter = $.ajax({
@@ -14,6 +17,8 @@ getter.done(function(responce){
   for (var i = 0; i < a.length; i++) {
     console.log(a[i].data.display_name);  //test
     $('#tabl').append("<tr><td>"+a[i].data.title+"</td><td><img id='hImg' src='"+a[i].data.header_img+"'</td><td>"+a[i].data.display_name+"</td></tr>")
+    $('#tabl').css({'height' : '100%','width' : '100%'})
+    $("#tabl").show();
 
   }
 
